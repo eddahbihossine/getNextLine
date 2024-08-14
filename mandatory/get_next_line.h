53f76d2e1b8df7c322ddba80afeaa866a8bb1bb5
eddahbihossine <eddahbi.hossine.solicode@gmail.com>
2024-08-14 06:38:38 +0100

    organize the master branch

---
+++ mandatory/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heddahbi <heddahbi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 02:51:52 by heddahbi          #+#    #+#             */
+/*   Updated: 2022/11/18 02:56:25 by heddahbi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+
+char	*ft_strjoin(char const	*s1, char const	*s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_line(char	**f, char	**line, int i);
+char	*rest(char	**f, int i);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *src);
+int		check_the_n(char	*buffer);
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+
+#endif
